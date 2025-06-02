f3aeda747be02806f8b2a5030aae0a3ba1be3313
Gokul Nath A <gokulnath0827@gmail.com>
2025-06-03 00:04:07 +0530

    fix: simplify error handling in audioContext and use const for variable declarations in base64ToArrayBuffer

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -34,7 +34,7 @@ export const audioContext: (
         map.set(options.id, ctx);
       }
       return ctx;
-    } catch (e) {
+    } catch {
       await didInteract;
       if (options?.id && map.has(options.id)) {
         const ctx = map.get(options.id);
@@ -52,8 +52,8 @@ export const audioContext: (
 })();
 
 export function base64ToArrayBuffer(base64: string) {
-  var binaryString = atob(base64);
-  var bytes = new Uint8Array(binaryString.length);
+  const binaryString = atob(base64);
+  const bytes = new Uint8Array(binaryString.length);
   for (let i = 0; i < binaryString.length; i++) {
     bytes[i] = binaryString.charCodeAt(i);
   }
