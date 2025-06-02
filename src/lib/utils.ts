3b46b9c5ac6b53a8224961dcde160fe340a1485d
Gokul Nath A <gokulnath0827@gmail.com>
2025-06-02 23:42:53 +0530

    feat: add GenAILiveClient class for managing live audio streaming and events

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -64,7 +64,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-function difference<T>(array: T[], ...values: T[][]): T[] {
+export function difference<T>(array: T[], ...values: T[][]): T[] {
   const excludeSet = new Set(values.flat());
   return array.filter(item => !excludeSet.has(item));
 }
