bb9c6b75e3f38a821c6f01d6cb5eedf498fbd106
Gokul Nath A <gokulnath0827@gmail.com>
2025-06-03 00:03:53 +0530

    fix: change handler return type from 'any' to 'void' in WorkletGraph type definition

--- src/lib/audioworklet-registry.ts
+++ src/lib/audioworklet-registry.ts
@@ -4,7 +4,7 @@
  */
 export type WorkletGraph = {
   node?: AudioWorkletNode;
-  handlers: Array<(this: MessagePort, ev: MessageEvent) => any>;
+  handlers: Array<(this: MessagePort, ev: MessageEvent) => void>;
 };
 
 export const registeredWorklets: Map<
